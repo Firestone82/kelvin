f29159d840934d64522ce788f8fb6eabe4ee2bad
Firestone82 <mikpav@email.cz>
2025-10-27 10:08:38 -0500

    Added LLM endpoint for uploading result to submit result folder.
    # Conflicts:
    #	web/urls.py
    #	web/views/llm.py
    #	web/views/utils.py

--- web/views/utils.py
+++ web/views/utils.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.core import signing
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from unidecode import unidecode
 
 from common.models import Submit
+from common.utils import is_teacher
 
 
 def file_response(file, filename: str, mimetype: str) -> HttpResponse:
@@ -12,10 +15,16 @@ def file_response(file, filename: str, mimetype: str) -> HttpResponse:
     return response
 
 
-def authenticate_submit_token_request(request, submit: Submit):
+def has_permission_for_submit(request, submit: Submit) -> None:
     if "token" in request.GET:
         token = signing.loads(request.GET["token"], max_age=3600)
-        if token.get("submit_id") == submit.id:
-            return
+        if token.get("submit_id") != submit.id:
+            raise PermissionDenied()
 
-    raise PermissionDenied()
+    if not request.user.is_authenticated:
+        return redirect(f"{settings.LOGIN_URL}?next={request.path}")
+
+    if not is_teacher(request.user) and request.user.username != submit.student.username:
+        raise PermissionDenied()
+
+    return None
